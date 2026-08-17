76649295cee6afcc81571d408a13118734081521
shinya <kobayashi_shinya@outlook.com>
2026-08-18 00:33:46 +0800

    chore: verify site ownership for Google Search Console

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,6 +31,7 @@ export default defineNuxtConfig({
 			meta: [
 				{ name: 'author', content: [blogConfig.author.name, blogConfig.author.email].filter(Boolean).join(', ') },
 				{ name: 'color-scheme', content: 'light dark' },
+				{ name: 'google-site-verification', content: 'Upwpz7OZi3RkL8sFzigeC7dGcmnZUO3bKqGLizsUl0w' },
 				// 此处为元数据的生成器标识，不建议修改
 				{ 'name': 'generator', 'content': `${pascalCase(packageJson.name)} ${packageJson.version}`, 'data-github-repo': packageJson.homepage },
 				{ name: 'mobile-web-app-capable', content: 'yes' },
